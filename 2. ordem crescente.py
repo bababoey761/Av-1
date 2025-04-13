9bf8c0871d061ab25d12767ce262294045da6b86
joaquim <joaquimpaulovieirademeloo@gmail.com>
2025-04-13 13:15:09 -0300

    Add files via upload

---
+++ 2. ordem crescente.py
@@ -0,0 +1,4 @@
+def cem_primeiros_naturais(): #define a função
+  for i in range(101): #variável i recebe o valor de 0 até 100
+    print(i) #imprime o valor atual de i
+cem_primeiros_naturais()  #chama a função  
