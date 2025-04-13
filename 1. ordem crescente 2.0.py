dcbf0015d05cae2b64ae95e476e046853cb77c85
joaquim <joaquimpaulovieirademeloo@gmail.com>
2025-04-13 17:55:35 -0300

    Add files via upload

---
+++ 1. ordem crescente 2.0.py
@@ -0,0 +1,9 @@
+
+numeros = list(map(int, input("Digite três números separados por espaço: ").split())) # Solicita três números inteiros em uma única linha, separados por espaço
+for i in range(3): # Verifica se foram digitados exatamente três números
+    if len(numeros) != 3:
+        print("Por favor, digite exatamente três números.")
+        exit()
+numeros.sort() # Ordena os números em ordem crescente
+
+print("Os números em ordem crescente são:", numeros) # Exibe os números em ordem crescente0
