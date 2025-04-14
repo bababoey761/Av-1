2c76b62be174f0c0e7153013a0a59ee42d2c3cdb
joca <joaquimpaulovieirademeloo@gmail.com>
2025-04-14 15:12:48 -0300

    6. inversão de string.py

--- 6. inversão de string.py
+++ 6. inversão de string.py
@@ -1,4 +1,3 @@
-
 texto = input("Digite uma palavra: ") # Solicita uma string ao usuário
 texto_invertido = texto[::-1] # Inverte o conteúdo da string
 print("palavra invertida:", texto_invertido) # Exibe a string invertida
