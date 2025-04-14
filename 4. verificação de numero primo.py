f993692b5d93fbcef201fd8403cbf8b9a4d5201a
joca <joaquimpaulovieirademeloo@gmail.com>
2025-04-14 15:22:11 -0300

    4. verificação de numero primo.py

--- 4. verificação de numero primo.py
+++ 4. verificação de numero primo.py
@@ -1,9 +1,9 @@
 numero = int(input("Digite um número: ")) # Solicita um número ao usuário
 
-if numero < 2: # Verifica se o número é primo
+if numero < 2: # Verifica se o número se encaixa na exceção do 1 e 0
     print("Número não é Primo")
 else:
-    primo = True # Inicializa a variável primo como True
+    primo = True
     for i in range(2, int(numero ** 1/2) + 1): # Verifica se o número é divisível por algum número entre 2 e a raiz quadrada do número
         if numero % i == 0: # se o número for divisível, não é primo
             primo = False 
