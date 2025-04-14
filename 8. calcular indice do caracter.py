673107db42a74d14f2777560dc7f51e63ca0cd8d
joca <joaquimpaulovieirademeloo@gmail.com>
2025-04-14 15:09:08 -0300

    Add files via upload

---
+++ 8. calcular indice do caracter.py
@@ -0,0 +1,7 @@
+# Solicita a string e o caractere
+string = input("Digite a string: ")
+caracter = input("Digite o caractere: ")
+
+indice = string.find(caracter) # Encontra o índice do caractere na string
+
+print(indice) # Imprime o índice do caractere
